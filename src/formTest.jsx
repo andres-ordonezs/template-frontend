4e3eb1de1b207acdc4c7e3895460d3e871a8584f
andres-ordonezs <andresmw.89@gmail.com>
2024-04-17 15:21:20 -0400

    file upload

--- src/formTest.jsx
+++ src/formTest.jsx
@@ -11,20 +11,24 @@ import React, {useState, useEffect} from "react";
  *
  */
 function FormTest({search}) {
-  const [formData, setformData] = useState("");
+  const [formData, setformData] = useState({});
   console.log("formTeststate: formData", formData);
 
   /** updates formData based on user input */
   function handleChange(evt) {
-    setformData(evt.target.value);
+    const selectedFile = evt.target.files[0]; ///
+    setformData(selectedFile); ///
+    // setformData(evt.target.value);  ///
   }
+  const formDataObj = new FormData();
+  formDataObj.append("file", formData); //
 
   /** Calls parent function to update parent's state with search formData */
   async function handleSubmit(evt) {
     evt.preventDefault();
     await fetch("http://localhost:3000/places/uploadImage", {
       method: "POST",
-      body: formData,
+      body: formDataObj,
     });
     setformData("");
   }
@@ -36,7 +40,6 @@ function FormTest({search}) {
         type="file"
         id="myfile"
         name="myfile"
-        value={formData}
         onChange={handleChange}
       ></input>
       <button>Upload</button>
